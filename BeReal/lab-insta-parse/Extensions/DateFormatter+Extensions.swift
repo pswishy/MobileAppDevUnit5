685a59b227c7a84172df28abe33b4a0f9cc07191
Pierce Medy <68874976+pswishy@users.noreply.github.com>
2024-03-26 15:35:11 -0400

    Add files via upload

---
+++ BeReal/lab-insta-parse/Extensions/DateFormatter+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  DateFormatter+Extensions.swift
+//
+
+import Foundation
+
+extension DateFormatter {
+    static var postFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        return formatter
+    }()
+}
